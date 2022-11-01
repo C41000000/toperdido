7a54580a2efd7c62891e053820b409487bdb44b6
caio <caio.santiago@doctum.edu.br>
2022-11-01 12:00:08 -0300

    controlador de login e cadastro

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -129,6 +129,9 @@
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                   </li>
                   <li class="nav-item">
+                    <a class="nav-link" href="{{route('login')}}">Login</a>
+                  </li>                  
+                  <li class="nav-item">
                     <a class="nav-link" href="{{route('locais')}}">Locais</a>
                   </li>
                   <li class="nav-item">
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/local/{cidade?}', [ToPerdidoController::class, 'locais'])->name('lo
 Route::get('/busca/{cidade}', [ToPerdidoController::class, 'buscaCidades']);
 Route::get('/detalhes/{local}', [ToPerdidoController::class, 'detalhes'])->name('detalhes');
 Route::get('/informacoes/{local}', [ToPerdidoController::class, 'buscaLocaisAutocomplete'])->name('informacoes');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::get('/mapa', function(){
     return view('mapa');
 })->name('mapa');
